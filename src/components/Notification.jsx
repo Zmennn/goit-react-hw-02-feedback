2f8ece72706f62398f2353c34dcc1427420c16b0
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-10-07 11:42:17 +0300

    1.3

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,5 +1,6 @@
 import style from "./FeedBack.module.css";
 
 export default function Notification({message}) {
-  return  <div className={style.resultString}>{message}</div>
+  return
+    <div className={style.resultString}>{message}</div>
 }
